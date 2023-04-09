53a7f5ebd251d30dda61b9aca7d0eb33eeb5d6ad
wonjong.yoo <dbdnjswhd111@gmail.com>
2023-04-09 13:54:49 +0900

    Git 로컬 변경 사항을 기반으로 영향 받는 모든 테스트를 실행시키는 Action 구현

---
+++ src/main/kotlin/com/github/wonjongyoo/testrunner/intention/GitLocalChangeBasedTestRunnerAction.kt
@@ -0,0 +1,121 @@
+package com.github.wonjongyoo.testrunner.intention
+
+import com.github.wonjongyoo.testrunner.runner.JunitTestRunner
+import com.intellij.codeInsight.actions.VcsFacade
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor
+import com.intellij.psi.PsiReference
+import com.intellij.psi.PsiReferenceExpression
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.idea.references.KtSimpleNameReference
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtNamedFunction
+
+class GitLocalChangeBasedTestRunnerAction: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val changeListManager = ChangeListManager.getInstance(project)
+        val psiManager = PsiManager.getInstance(project)
+
+        // 1. 변경사항 내의 모든 PsiMethod 를 찾는다.
+        // 2. PsiMethod 내의 모든 참조를 찾는다.
+        //   2-1. 만약 그 참조가 테스트라면(여기서는 KtSimpleNameReference) 테스트 대상에 추가
+        //   2-2 만약 그 참조가 일반 메서드라면 (2)를 반복
+        // 3. (2-1) 의 결과를 묶어서 테스트 실행
+        val visitedPsiMethods: MutableSet<PsiMethod> = mutableSetOf()
+        val targets = changeListManager.changeLists
+            .flatMap { it.changes }
+            .map { change ->
+                val virtualFile = change.afterRevision?.file?.virtualFile ?: return@map listOf<PsiMethod>()
+                val psiFile: PsiFile = psiManager.findFile(virtualFile) ?: return@map listOf<PsiMethod>()
+                val allChangedRanges = VcsFacade.getInstance().getChangedRangesInfo(psiFile)?.allChangedRanges ?: return@map listOf<PsiMethod>()
+
+                val visitor = PsiMethodVisitor(allChangedRanges)
+                psiFile.accept(visitor)
+
+                visitor.psiMethods
+            }
+            .flatten()
+            .map { targetMethods ->
+
+                val target: Map<KtClass, List<KtNamedFunction>> = findTestMethodsRecursively(project, targetMethods, visitedPsiMethods)
+
+                println("target : $target")
+                return@map target
+            }
+            .flatMap { it.entries }
+            .groupBy({ it.key }, { it.value })
+            .mapValues { (_, values) -> values.flatten().distinct() }
+
+        JunitTestRunner.runTestMethod(project, targets)
+    }
+
+    private fun findTestMethodsRecursively(
+        project: Project,
+        psiMethod: PsiMethod,
+        visitedPsiMethods: MutableSet<PsiMethod>
+    ): Map<KtClass, List<KtNamedFunction>> {
+        if (visitedPsiMethods.contains(psiMethod)) {
+            println("already visited PsiMethod : ${psiMethod.name}")
+            return mapOf()
+        }
+
+        val references = MethodReferenceFinder.findReferences(psiMethod, project)
+
+        // Resovle refereces
+        val testMethods: List<KtNamedFunction> = references
+            .filterIsInstance<KtSimpleNameReference>()
+            .mapNotNull { PsiTreeUtil.getParentOfType(it.element, KtNamedFunction::class.java) }
+
+        if (testMethods.isEmpty()) {
+            println("no test method exist in ${psiMethod.name}")
+            return mapOf()
+        }
+
+        val testClass = PsiTreeUtil.getParentOfType(testMethods.first(), KtClass::class.java) ?: return mapOf()
+
+        val referenceMethods = references
+            .filterIsInstance<PsiReferenceExpression>()
+            .mapNotNull { PsiTreeUtil.getParentOfType(it.element, PsiMethod::class.java) }
+
+        visitedPsiMethods.add(psiMethod)
+        return if (referenceMethods.isNotEmpty()) {
+            mapOf(testClass to testMethods) + referenceMethods.flatMap { findTestMethodsRecursively(project, it, visitedPsiMethods).entries }.associate { it.key to it.value }
+        } else {
+            mapOf(testClass to testMethods)
+        }
+    }
+
+    private class MethodReferenceFinder {
+        companion object {
+            fun findReferences(psiMethod: PsiMethod, project: Project): Collection<PsiReference> {
+                val searchScope: GlobalSearchScope = GlobalSearchScope.projectScope(project)
+                return ReferencesSearch.search(psiMethod, searchScope).findAll()
+            }
+        }
+    }
+
+    private class PsiMethodVisitor(private val textRanges: List<TextRange>) : PsiRecursiveElementWalkingVisitor() {
+        val psiMethods = mutableListOf<PsiMethod>()
+
+        override fun visitElement(element: PsiElement) {
+            if (element is PsiMethod && textRanges.any { element.textRange.contains(it) }) {
+                if (!psiMethods.contains(element)) {
+                    psiMethods.add(element)
+                }
+            }
+
+            super.visitElement(element)
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/wonjongyoo/testrunner/runner/JunitTestRunner.kt
@@ -0,0 +1,67 @@
+package com.github.wonjongyoo.testrunner.runner
+
+import com.intellij.execution.RunManager
+import com.intellij.execution.executors.DefaultRunExecutor
+import com.intellij.execution.junit.JUnitConfiguration
+import com.intellij.execution.junit.JUnitConfigurationType
+import com.intellij.execution.runners.ExecutionUtil
+import com.intellij.execution.testframework.TestSearchScope
+import com.intellij.openapi.module.ModuleUtilCore
+import com.intellij.openapi.project.Project
+import com.jetbrains.rd.util.first
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtNamedFunction
+
+class JunitTestRunner {
+    companion object {
+        fun runTestMethod(
+            project: Project,
+            testMethodListByTestClass: Map<KtClass, List<KtNamedFunction>>
+        ) {
+            val testMethodName = "Run all tests in locally change files"
+
+            val runManager = RunManager.getInstance(project)
+            val setting = runManager.createConfiguration(
+                testMethodName, JUnitConfigurationType::class.java
+            )
+
+            val runConfiguration = setting.configuration
+            if (runConfiguration is JUnitConfiguration) {
+                val jUnitConfiguration = runConfiguration
+
+                val firstTestClass = testMethodListByTestClass.first().key
+                val module = ModuleUtilCore.findModuleForPsiElement(firstTestClass)
+                if (module != null) {
+                    jUnitConfiguration.setSearchScope(TestSearchScope.WHOLE_PROJECT)
+                    jUnitConfiguration.setModule(module)
+                }
+
+                val persistentData = jUnitConfiguration.persistentData
+                persistentData.TEST_OBJECT = JUnitConfiguration.TEST_PATTERN
+
+                persistentData.setPatterns(generateTestPattern(testMethodListByTestClass))
+
+                val executor = DefaultRunExecutor.getRunExecutorInstance()
+                ExecutionUtil.runConfiguration(setting, executor)
+            }
+        }
+
+        private fun generateTestPattern(
+            testMethodListByTestClass: Map<KtClass, List<KtNamedFunction>>
+        ): LinkedHashSet<String> {
+            val testPatterns = testMethodListByTestClass.map {
+                val testClass = it.key
+                val testMethods = it.value
+
+
+                val testClassFqName = testClass.fqName.toString()
+                testMethods.map {
+                    "$testClassFqName,${it.name}"
+                }
+            }
+                .flatMap { it }
+
+            return LinkedHashSet(testPatterns)
+        }
+    }
+}
