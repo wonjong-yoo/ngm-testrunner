f9e97c5c9dbf959f90b0ee95e40c412704fc7ba0
wonjong.yoo <dbdnjswhd111@gmail.com>
2023-11-03 23:37:23 +0900

    ToolWindow 아이콘 추가

--- src/main/kotlin/com/github/wonjongyoo/ngm/icons/NgmTestRunnerIcons.kt
+++ src/main/kotlin/com/github/wonjongyoo/ngm/icons/NgmTestRunnerIcons.kt
@@ -5,4 +5,5 @@ import javax.swing.Icon
 
 object NgmTestRunnerIcons {
     val icon: Icon = IconLoader.getIcon("META-INF/icons/run2.svg", NgmTestRunnerIcons::class.java)
+    val toolWindowIcon: Icon = IconLoader.getIcon("META-INF/icons/tool_window.svg", NgmTestRunnerIcons::class.java)
 }
--- src/main/kotlin/com/github/wonjongyoo/ngm/window/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/wonjongyoo/ngm/window/MyToolWindowFactory.kt
@@ -1,5 +1,6 @@
 package com.github.wonjongyoo.ngm.window
 
+import com.github.wonjongyoo.ngm.icons.NgmTestRunnerIcons
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
@@ -9,5 +10,6 @@ class MyToolWindowFactory : ToolWindowFactory {
         val myToolWindow = MyToolWindow(toolWindow)
         val content = toolWindow.contentManager.factory.createContent(myToolWindow, "All Affected Methods And Tests", false)
         toolWindow.contentManager.addContent(content)
+        toolWindow.setIcon(NgmTestRunnerIcons.toolWindowIcon)
     }
 }
